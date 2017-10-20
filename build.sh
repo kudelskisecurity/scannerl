08655f9fb9c65471408649012874c0545b3ec634
deadc0de6 <info@deadc0de.ch>
2017-10-20 13:58:56 +0200

    refactor building scripts

--- build.sh
+++ build.sh
@@ -1,19 +1,34 @@
 #!/bin/sh
-if [ $(dpkg-query -W -f='${Status}' erlang 2> /dev/null | grep -c "ok installed") -eq 0 ]; then
-  echo "Install erlang"
-elif [ $(erl -eval 'erlang:display(list_to_integer(erlang:system_info(otp_release))), halt().'  -noshell) -lt 18 ]; then
-  echo "Your erlang version is too old, you should update it"
-elif [ $(dpkg-query -W -f='${Status}' erlang-src 2> /dev/null | grep -c "ok installed") -eq 0 ]; then
-  echo "Install erlang-src"
-elif hash rebar 2>/dev/null; then
-  # Updating fpmodules list
-  for i in `ls -1 ./src/fpmodules -I fp_module.erl`; do x="${i%.*erl}"; echo $x | sed 's/^fp_//g'; done | tr '\n' ',' | sed 's/^/\-define(FP_MODULES_LIST, [/' | sed 's/,$/])./' > ./src/includes/fpmodules.hrl
-  # Updating outmodules list
-  for i in `ls -1 ./src/outmodules -I out_behavior.erl`; do x="${i%.*erl}"; echo $x | sed 's/^out_//g'; done | tr '\n' ',' | sed 's/^/\-define(OUT_MODULES_LIST, [/' | sed 's/,$/])./' > ./src/includes/outmodules.hrl
-  # update git hash
-  echo "-define(GIT_SHORT_HASH, \"`git rev-parse --short HEAD`\")." > ./src/includes/githash.hrl
-  # Compile
-  rebar compile escriptize
-else
-  echo "Install rebar"
-fi
+# https://github.com/kudelskisecurity/scannerl
+
+# check deps
+hash erl 2>/dev/null || { echo 'install \"erlang\" (https://github.com/kudelskisecurity/scannerl)'; exit 1; }
+hash rebar 2>/dev/null || { echo 'install \"rebar\" (https://github.com/kudelskisecurity/scannerl)'; exit 1; }
+
+# check erlang version
+erlversion=`erl -eval 'erlang:display(list_to_integer(erlang:system_info(otp_release))), halt().'  -noshell \
+  | tr -d '\r'`
+[ "${erlversion}" -lt "18" ] && echo "Your erlang version is too old, you should update it" && exit 1
+
+# Updating fpmodules list
+for i in `ls -1 ./src/fpmodules -I fp_module.erl`; do
+  echo ${i%.*erl} | sed 's/^fp_//g'
+done | tr '\n' ',' | \
+       sed 's/^/\-define(FP_MODULES_LIST, [/' | \
+       sed 's/,$/])./' > ./src/includes/fpmodules.hrl
+
+# Updating outmodules list
+for i in `ls -1 ./src/outmodules -I out_behavior.erl`; do
+  echo ${i%.*erl} | sed 's/^out_//g'
+done | tr '\n' ',' | \
+       sed 's/^/\-define(OUT_MODULES_LIST, [/' | \
+       sed 's/,$/])./' > ./src/includes/outmodules.hrl
+
+# update git hash
+gitv=`git rev-parse --short HEAD 2>/dev/null`
+echo "-define(GIT_SHORT_HASH, \"${gitv}\")." > ./src/includes/githash.hrl 2>/dev/null
+echo "-define(ERLANG_VERSION, \"${erlversion}\")." > ./src/includes/erlversion.hrl
+[ "${erlversion}" -lt "20" ] && echo "-define(USE_GENFSM, true)." >> ./src/includes/erlversion.hrl
+
+# Compile
+rebar compile escriptize
