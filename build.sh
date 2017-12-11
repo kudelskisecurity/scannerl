07087447ed0ac61d51527d7f54f3b5442b8e06e6
deadc0de6 <info@deadc0de.ch>
2017-12-11 16:34:23 +0100

    bump version

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # https://github.com/kudelskisecurity/scannerl
-version="0.36"
+version="0.37"
 
 # check deps
 hash erl 2>/dev/null || { echo "install \"erlang\" (https://github.com/kudelskisecurity/scannerl)"; exit 1; }
